b2d829633f21a54a39828ee2c75d8149e432a276
crossjas_mac <crossjae@163.com>
2019-02-28 23:01:03 +0800

    feat: 设定目标

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # 深入浅出webpack
 
-##log
-* 2019/2/27 初始化项目
+[] 单h5页面：针对活动页，比如只需要一张背景图，一个按钮，几行文字，或者引入了swiper的整屏滑动页面
+
+[] 简单的配置 - entry / output
+[] loader配置 - 识别css模块
+[] plugin配置 - 将css模块单独抽离
+[] webpack-dev-server
+[] hot
+[] source map
+[] babel
+    [] 用es5的prototype实现es6的class
--- dist/bundle.js
+++ dist/bundle.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("\nvar content = __webpack_require__(/*! !./node_modules/css-loader/dist/cjs.js!./main.css */ \"./node_modules/css-loader/dist/cjs.js!./main.css\");\n\nif(typeof content === 'string') content = [[module.i, content, '']];\n\nvar transform;\nvar insertInto;\n\n\n\nvar options = {\"hmr\":true}\n\noptions.transform = transform\noptions.insertInto = undefined;\n\nvar update = __webpack_require__(/*! ./node_modules/style-loader/lib/addStyles.js */ \"./node_modules/style-loader/lib/addStyles.js\")(content, options);\n\nif(content.locals) module.exports = content.locals;\n\nif(false) {}\n\n//# sourceURL=webpack:///./main.css?");
+eval("\nvar content = __webpack_require__(/*! !./node_modules/css-loader/dist/cjs.js!./main.css */ \"./node_modules/css-loader/dist/cjs.js!./main.css\");\n\nif(typeof content === 'string') content = [[module.i, content, '']];\n\nvar transform;\nvar insertInto;\n\n\n\nvar options = {\"insertAt\":\"top\",\"hmr\":true}\n\noptions.transform = transform\noptions.insertInto = undefined;\n\nvar update = __webpack_require__(/*! ./node_modules/style-loader/lib/addStyles.js */ \"./node_modules/style-loader/lib/addStyles.js\")(content, options);\n\nif(content.locals) module.exports = content.locals;\n\nif(false) {}\n\n//# sourceURL=webpack:///./main.css?");
 
 /***/ }),
 
@@ -115,7 +115,7 @@ eval("const show = __webpack_require__(/*! ./show.js */ \"./show.js\");\n__webpa
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("exports = module.exports = __webpack_require__(/*! ./node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.i, \"#app{\\n    text-align: center;\\n}\", \"\"]);\n\n\n\n//# sourceURL=webpack:///./main.css?./node_modules/css-loader/dist/cjs.js");
+eval("exports = module.exports = __webpack_require__(/*! ./node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.i, \"#app{\\n    text-align: center;\\n    font-size: 20px;\\n    color: blue;\\n}\", \"\"]);\n\n\n\n//# sourceURL=webpack:///./main.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
